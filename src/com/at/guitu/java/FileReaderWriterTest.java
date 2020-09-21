72c1227158bca6ec6ae7c9ff1079daedac1b0890
z662388200 <914281275@qq.com>
2020-09-21 11:45:51 +0800

    Commit Message

---
+++ src/com/at/guitu/java/FileReaderWriterTest.java
@@ -0,0 +1,20 @@
+package com.at.guitu.java;
+
+/**
+ * 一、流的分类
+ * 1.操作数据单位：字节流、字符流
+ * 2.数据的流向：输入流、输出流
+ * 3.流的角色：节点流、处理流
+ *
+ *
+ * 二、流的体系结构
+ *抽象基类                 节点流（或文件流）       缓冲流（处理流的一种）
+ * InputStream            FileInputStream       BufferedInputStream
+ * OutputStream           FileOutputStream      BufferedOutputStream
+ * Reader                 FileReader            BufferedReader
+ * Writer                 FileWriter            BufferedWriter
+ *
+ */
+public class FileReaderWriterTest {
+
+}
